c62e4daede8f86b2c62a187ef6ce25a0ac940756
sergiogomesan8 <102580112+sergiogomesan8@users.noreply.github.com>
2022-04-07 14:35:20 +0200

    EjercicioFecha

---
+++ src/main/java/com/formacion/nttdata/hello/model/User.java
@@ -0,0 +1,31 @@
+package com.formacion.nttdata.hello.model;
+
+public class User {
+	String userName;
+	String userSurname;
+	int numero;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserSurname() {
+		return userSurname;
+	}
+	public void setUserSurname(String userSurname) {
+		this.userSurname = userSurname;
+	}
+	
+	
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+}
